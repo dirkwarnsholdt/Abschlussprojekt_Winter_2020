203cbcf7b2166482e8352de305bef38355fb9f72
JannisRex <rex.jannis@web.de>
2020-03-18 14:53:41 +0100

    [added] require for custom fonts, which should show error missing fonts
    Signed-off-by: JannisRex <rex.jannis@web.de>

--- App.tsx
+++ App.tsx
@@ -1,10 +1,11 @@
 import React from 'react'
-import { Text } from 'react-native'
-import { Container } from './src/components/index'
+import { Container, Text } from './src/components/index'
 
 export default function App(): JSX.Element {
   return (
     <Container>
+      <Text type='title'>Title - Impact Font</Text>
+      <Text type='Caption'>Light Lato Caption text</Text>
       <Text>Open up App.tsx to start working on your app!</Text>
     </Container>
   )
