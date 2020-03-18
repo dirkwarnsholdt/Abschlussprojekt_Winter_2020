2d13518036307d1b9efc523fc2fbbb062f27bd5c
JannisRex <rex.jannis@web.de>
2020-03-18 16:51:05 +0100

    [merged] to feature/SwitchNavigator
    Signed-off-by: JannisRex <rex.jannis@web.de>

--- src/components/Text/main.tsx
+++ src/components/Text/main.tsx
@@ -21,7 +21,41 @@ const customText = ({ children, type }: Props): JSX.Element => {
         {children}
       </Text>
     )
+  }import React from 'react'
+import { Text } from 'react-native'
+import styles from './styles'
+
+type Props = {
+  children: string,
+  type?: string
+}
+
+const customText = ({ children, type }: Props): JSX.Element => {
+  if (type === 'title' || type === 'Title') {
+    return (
+      <Text style={styles.textTitle}>
+        {children}
+      </Text>
+    )
   }
+  if (type === 'caption' || type === 'Caption') {
+    return (
+      <Text style={styles.textCaption}>
+        {children}
+      </Text>
+    )
+  }
+
+  return (
+    <Text style={styles.textBody}>
+      {children}
+    </Text>
+  )
+}
+
+export default customText
+
+
   return (
     <Text style={styles.textBody}>
       {children}
