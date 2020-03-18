900667eb9432e8b4c81a4afc76a58c60278c15e7
JannisRex <rex.jannis@web.de>
2020-03-18 13:32:51 +0100

    [created] with boilerplate code
    Signed-off-by: JannisRex <rex.jannis@web.de>

---
+++ src/components/Text/main.tsx
@@ -0,0 +1,11 @@
+import React from 'react'
+import styles from './styles'
+
+type Props = {
+
+}
+
+const Text = (: Props): JSX.Element => (
+
+)
+export default Text
---
+++ src/components/Text/styles.ts
@@ -0,0 +1,6 @@
+import { StyleSheet } from 'react-native'
+import theme from '../../config/theme.style'
+
+export default StyleSheet.create({
+
+})
