d11157b26b8a6c5d6351d787aeee34e56f752201
JannisRex <rex.jannis@web.de>
2020-03-18 14:43:58 +0100

    [added] styles + conditional rendering + optional type prop
    Signed-off-by: JannisRex <rex.jannis@web.de>

--- src/components/Text/main.tsx
+++ src/components/Text/main.tsx
@@ -1,11 +1,32 @@
 import React from 'react'
+import { Text } from 'react-native'
 import styles from './styles'
 
 type Props = {
-
+  children: string,
+  type?: string
 }
 
-const Text = (: Props): JSX.Element => (
+const customText = ({ children, type }: Props): JSX.Element => {
+  if (type === 'title' || type === 'Title') {
+    return (
+      <Text style={styles.textTitle}>
+        {children}
+      </Text>
+    )
+  }
+  if (type === 'caption' || type === 'Caption') {
+    return (
+      <Text style={styles.textCaption}>
+        {children}
+      </Text>
+    )
+  }
+  return (
+    <Text style={styles.textBody}>
+      {children}
+    </Text>
+  )
+}
 
-)
-export default Text
+export default customText
--- src/components/Text/styles.ts
+++ src/components/Text/styles.ts
@@ -2,5 +2,31 @@ import { StyleSheet } from 'react-native'
 import theme from '../../config/theme.style'
 
 export default StyleSheet.create({
-
+  textTitle: {
+    alignSelf: 'center',
+    textAlign: 'center',
+    fontFamily: theme.FONT_FAMILY_TITLE,
+    fontSize: theme.FONT_SIZE_LARGE,
+    fontWeight: theme.FONT_WEIGHT_MEDIUM,
+    fontStyle: 'normal',
+    color: theme.COLOR_IVORY
+  },
+  textCaption: {
+    alignSelf: 'center',
+    textAlign: 'center',
+    fontFamily: theme.FONT_FAMILY_CAPTION,
+    fontSize: theme.FONT_SIZE_SMALL,
+    fontWeight: theme.FONT_WEIGHT_MEDIUM,
+    fontStyle: 'normal',
+    color: theme.COLOR_IVORY
+  },
+  textBody: {
+    alignSelf: 'center',
+    textAlign: 'center',
+    fontFamily: theme.FONT_FAMILY_TEXT,
+    fontSize: theme.FONT_SIZE_TINY,
+    fontWeight: theme.FONT_WEIGHT_LIGHT,
+    fontStyle: 'normal',
+    color: theme.COLOR_IVORY
+  }
 })
