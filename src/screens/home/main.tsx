e95ac0b571c4fb984fc9f473d633f870639f6f31
JannisRex <rex.jannis@web.de>
2020-03-18 14:57:59 +0100

    [added] HomeSCreen as default navigate after loading screen whihc is at App.tsx and cant return back
    Signed-off-by: JannisRex <rex.jannis@web.de>

---
+++ src/screens/home/main.tsx
@@ -0,0 +1,16 @@
+import React, { Component } from 'react'
+import styles from './styles'
+
+class HomeScreen extends Component {
+  render() {
+    return (
+      <Container>
+        <Text type='title'>Title - Impact Font</Text>
+        <Text type='Caption'>Light Lato Caption text</Text>
+        <Text>Open up App.tsx to start working on your app!</Text>
+      </Container>
+    )
+  }
+}
+
+export default HomeScreen
