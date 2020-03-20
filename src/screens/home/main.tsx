22a2aeed06d14cb0b8738ee6ec852ded4f863819
JannisRex <rex.jannis@web.de>
2020-03-20 22:29:53 +0100

    [added] HomeImage to HomeScreen as direct import
    Signed-off-by: JannisRex <rex.jannis@web.de>

--- src/screens/home/main.tsx
+++ src/screens/home/main.tsx
@@ -1,5 +1,5 @@
 import React, { Component } from 'react'
-import Container from '../../components/Container/main'
+import { Container, HomeImage } from '../../components/index'
 import Text from '../../components/Text/main'
 // import styles from './styles'
 
@@ -7,6 +7,7 @@ class HomeScreen extends Component {
   render(): JSX.Element {
     return (
       <Container>
+        <HomeImage />
         <Text type='title'>Title - Impact Font</Text>
         <Text type='Caption'>Light Lato Caption text</Text>
         <Text>Open up App.tsx to start working on your app!</Text>
