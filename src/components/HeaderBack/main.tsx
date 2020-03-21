3a4bd4067bc99d940c73ceb457820b2496d88584
JannisRex <rex.jannis@web.de>
2020-03-21 20:02:28 +0100

    [added] LeftIcon = BackArrow as different Header
    Signed-off-by: JannisRex <rex.jannis@web.de>

---
+++ src/components/HeaderBack/main.tsx
@@ -0,0 +1,21 @@
+import React, { Component } from 'react'
+import { Header } from 'react-native-elements'
+import BackIcon from '../BackIcon/main'
+import styles from './styles'
+import Logo from '../Logo/main'
+
+class CustomHeader extends Component<> {
+  render(): JSX.Element {
+    return (
+      <Header
+        style={{ flex: 1 }}
+        barStyle='dark-content'
+        leftComponent={<BackIcon />}
+        centerComponent={<Logo />}
+        containerStyle={styles.container}
+      />
+    )
+  }
+}
+
+export default CustomHeader
