fa226c16906d2b5c782e49c693663f81c0e875af
JannisRex <rex.jannis@web.de>
2020-03-21 19:15:18 +0100

    [added] MenuIcon to Import/Exports
    Signed-off-by: JannisRex <rex.jannis@web.de>

--- src/components/index.ts
+++ src/components/index.ts
@@ -1,4 +1,5 @@
 export { default as Container } from './Container/main'
+export { default as DrawerIcon } from './DrawerIcon/main'
 export { default as Header } from './Header/main'
 export { default as HomeImage } from './HomeImage/main'
 export { default as Text } from './Text/main'
