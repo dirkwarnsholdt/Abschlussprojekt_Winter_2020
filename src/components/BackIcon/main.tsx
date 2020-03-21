f8f6964daa3db2d683337aebfe88a6f70a461a7c
JannisRex <rex.jannis@web.de>
2020-03-21 19:58:40 +0100

    [added] Left-Arrow Icon with onTouch() functionalty => Go Back 1 page ----> For later Offers->Offer->Detail pathing
    Signed-off-by: JannisRex <rex.jannis@web.de>

---
+++ src/components/BackIcon/main.tsx
@@ -0,0 +1,32 @@
+import React, { Component } from 'react'
+import { TouchableOpacity, Platform } from 'react-native'
+import { withNavigation, NavigationScreenProp } from 'react-navigation'
+import { Ionicons } from '@expo/vector-icons'
+import theme from '../../config/theme.style'
+
+type Props = {
+  navigation: NavigationScreenProp<any>
+}
+
+class BackIcon extends Component<Props> {
+  render() {
+    const { navigation } = this.props
+    return (
+      <TouchableOpacity
+        key={BackIcon.id}
+        onPress={ () => navigation.goBack() }>
+        <Ionicons
+          name={
+            Platform.OS === 'ios'
+              ? `ios-arrow-round-back`
+              : 'md-arrow-back'
+          }
+          size={30}
+          color={theme.COLOR_WHITE}
+        />
+      </TouchableOpacity>
+    )
+  }
+}
+
+export default withNavigation(BackIcon)
