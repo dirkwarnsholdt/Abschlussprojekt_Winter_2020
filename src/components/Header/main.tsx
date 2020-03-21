1b9d105b827edbbfebeb94db1326204f4437bef7
JannisRex <rex.jannis@web.de>
2020-03-21 17:18:43 +0100

    [imported] and adjusted default Header
    Signed-off-by: JannisRex <rex.jannis@web.de>

--- src/components/Header/main.tsx
+++ src/components/Header/main.tsx
@@ -6,7 +6,12 @@ import Logo from '../Logo/main'
 class CustomHeader extends Component {
   render(): JSX.Element {
     return (
-
+      <Header
+        style={{ flex: 1 }}
+        barStyle='dark-content'
+        centerComponent={<Logo />}
+        containerStyle={styles.container}
+      />
     )
   }
 }
