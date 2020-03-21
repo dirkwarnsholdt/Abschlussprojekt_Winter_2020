4578ac9b04a5c4632a4e0cf700e5fee5f8aefe8e
JannisRex <rex.jannis@web.de>
2020-03-21 17:17:41 +0100

    [added]container styles with darker BG compared to body
    Signed-off-by: JannisRex <rex.jannis@web.de>

---
+++ src/components/Header/styles.ts
@@ -0,0 +1,13 @@
+import { StyleSheet } from 'react-native'
+import theme from '../../config/theme.style'
+
+const styles = StyleSheet.create({
+  container: {
+    backgroundColor: theme.COLOR_HEEADER,
+    justifyContent: 'center',
+    alignItems: 'center',
+    alignContent: 'center'
+  }
+})
+
+export default styles
