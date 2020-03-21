55528f2a0e8cda09a724c6537d1f4e3e78eef17d
JannisRex <rex.jannis@web.de>
2020-03-21 19:53:13 +0100

    [adjusted] to new imports and fixed colors
    Signed-off-by: JannisRex <rex.jannis@web.de>

--- src/components/DrawerItem/main.tsx
+++ src/components/DrawerItem/main.tsx
@@ -1,6 +1,5 @@
 import React, { Component } from 'react'
-import { View, Text } from 'react-native'
-import { SafeAreaView } from '@react-navigation/native'
+import { View, SafeAreaView, Text } from 'react-native'
 import TouchableItem from './TouchableItem'
 import styles from './styles'
 import strings from '../../config/strings'
@@ -13,85 +12,74 @@ class DrawerItem extends Component {
     this.state = {
       isFinishedSaving: false,
       activeTintColor: null,
-      activeBackgroundColor: null,
-      inactiveTintColor: null,
-      inactiveBackgroundColor: null
+      activeBackgroundColor: theme.COLOR_PRIMARY,
+      inactiveTintColor: theme.COLOR_BACKGROUND,
+      inactiveBackgroundColor: theme.COLOR_BACKGROUND
     }
   }
 
-_saveToState: Function = (activeBackgroundColor: string, activeTintColor: string): null => {
-  if (!this.state.isFinishedSaving) {
-    this.state.isFinishedSaving = true
-    this.setState({
-      activeBackgroundColor,
-      activeTintColor,
-      inactiveBackgroundColor: activeTintColor
-    })
-  }
-}
-
-render(): JSX.Element {
-  return (
-    <>
-      <View style={{ backgroundColor: theme.COLOR_HEADER }}><Text style={styles.header}>{strings.drawer.header}</Text></View>
-      <View style={[styles.container, this.props.itemsContainerStyle, { backgroundColor: theme.COLOR_BACKGROUND }]}>
-        {this.props.items.map((route: any, index: number) => {
-          const focused = this.props.activeItemKey === route.key
-          const color = focused ? this.props.activeTintColor : this.props.inactiveTintColor
-          const backgroundColor = focused
-            ? this.state.activeBackgroundColor
-            : this.state.inactiveBackgroundColor
-          const scene = { route, index, focused, tintColor: color }
-          const icon = this.props.renderIcon(scene)
-          const label = this.props.getLabel(scene)
-          const accessibilityLabel = typeof label === 'string' ? label : undefined
-          const extraLabelStyle = focused ? this.props.activeLabelStyle : this.props.inactiveLabelStyle
-          return (
-            <TouchableItem
-              key={route.key}
-              accessible
-              accessibilityLabel={accessibilityLabel}
-              onPress={(): null => {
-                this.props.onItemPress({ route, focused })
-              }}
-              delayPressIn={0}
-            >
-              <SafeAreaView
-                style={[{ backgroundColor }, styles.item, this.props.itemStyle]}
-                forceInset={{
-                  [this.props.drawerPosition]: 'always',
-                  [this.props.drawerPosition === 'left' ? 'right' : 'left']: 'never',
-                  vertical: 'never'
+  render(): JSX.Element {
+    return (
+      <>
+        <View style={{ backgroundColor: theme.COLOR_HEADER }}><Text style={styles.header}>{strings.drawer.header}</Text></View>
+        <View style={[styles.container, this.props.itemsContainerStyle, { backgroundColor: theme.COLOR_BACKGROUND }]}>
+          {this.props.items.map((route: any, index: number) => {
+            const focused = this.props.activeItemKey === route.key
+            const color = focused ? this.props.activeTintColor : this.props.inactiveTintColor
+            const backgroundColor = focused
+              ? this.state.activeBackgroundColor
+              : this.state.inactiveBackgroundColor
+            const scene = { route, index, focused, tintColor: color }
+            const icon = this.props.renderIcon(scene)
+            const label = this.props.getLabel(scene)
+            const accessibilityLabel = typeof label === 'string' ? label : undefined
+            const extraLabelStyle = focused ? this.props.activeLabelStyle : this.props.inactiveLabelStyle
+            return (
+              <TouchableItem
+                key={route.key}
+                accessible
+                accessibilityLabel={accessibilityLabel}
+                onPress={(): null => {
+                  this.props.onItemPress({ route, focused })
                 }}
+                delayPressIn={0}
               >
-                {icon ? (
-                  <View
-                    style={[
-                      styles.icon,
-                      focused ? null : styles.inactiveIcon,
-                      this.props.iconContainerStyle
-                    ]}
-                  >
-                    {icon}
-                  </View>
-                ) : null}
-                {typeof label === 'string' ? (
-                  <Text
-                    style={[styles.label, { color }, this.props.labelStyle, extraLabelStyle]}
-                  >
-                    {label}
-                  </Text>
-                ) : (
-                  label
-                )}
-              </SafeAreaView>
-            </TouchableItem>
-          )
-        })}
-      </View>
-    </>
-  )
-}
+                <SafeAreaView
+                  style={[{ backgroundColor }, styles.item, this.props.itemStyle]}
+                  forceInset={{
+                    [this.props.drawerPosition]: 'always',
+                    [this.props.drawerPosition === 'left' ? 'right' : 'left']: 'never',
+                    vertical: 'never'
+                  }}
+                >
+                  {icon ? (
+                    <View
+                      style={[
+                        styles.icon,
+                        focused ? null : styles.inactiveIcon,
+                        this.props.iconContainerStyle
+                      ]}
+                    >
+                      {icon}
+                    </View>
+                  ) : null}
+                  {typeof label === 'string' ? (
+                    <Text
+                      style={[styles.label, { color }, this.props.labelStyle, extraLabelStyle]}
+                    >
+                      {label}
+                    </Text>
+                  ) : (
+                    label
+                  )}
+                </SafeAreaView>
+              </TouchableItem>
+            )
+          })}
+        </View>
+      </>
+    )
+  }
 }
 
 export default DrawerItem
