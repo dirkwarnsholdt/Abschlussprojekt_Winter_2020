ddbea4b55f516b6fd4d87e92664bd242450c8d87
JannisRex <rex.jannis@web.de>
2020-03-21 17:17:54 +0100

    [changed] class name to be PascalCased
    Signed-off-by: JannisRex <rex.jannis@web.de>

---
+++ src/components/Header/main.tsx
@@ -0,0 +1,14 @@
+import React, { Component } from 'react'
+import { Header } from 'react-native-elements'
+import styles from './styles'
+import Logo from '../Logo/main'
+
+class CustomHeader extends Component {
+  render(): JSX.Element {
+    return (
+
+    )
+  }
+}
+
+export default CustomHeader
