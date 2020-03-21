d5e1dd06e18c1d7d34e9b75195d65279dcbaaef2
JannisRex <rex.jannis@web.de>
2020-03-21 17:28:48 +0100

    [typo]
    Signed-off-by: JannisRex <rex.jannis@web.de>

--- src/components/Header/styles.ts
+++ src/components/Header/styles.ts
@@ -3,7 +3,7 @@ import theme from '../../config/theme.style'
 
 const styles = StyleSheet.create({
   container: {
-    backgroundColor: theme.COLOR_HEEADER,
+    backgroundColor: theme.COLOR_HEADER,
     justifyContent: 'center',
     alignItems: 'center',
     alignContent: 'center'
