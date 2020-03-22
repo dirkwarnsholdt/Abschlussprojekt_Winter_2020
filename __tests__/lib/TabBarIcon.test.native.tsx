db6886c9eb983254f03e7eb7aafde41befda4d04
JannisRex <rex.jannis@web.de>
2020-03-22 20:01:24 +0100

    [Jest] Test for TabBarIcon with all 3 examples
    Signed-off-by: JannisRex <rex.jannis@web.de>

---
+++ __tests__/lib/TabBarIcon.test.native.tsx
@@ -0,0 +1,39 @@
+import React from 'react'
+import TabBarIcon from '../../src/lib/TabBarIcon'
+import { mount } from 'enzyme'
+
+describe('renders MaterialIcons/adb correctly', () => {
+  it('should match Snapshot', () => {
+    const wrapper = mount(
+      <TabBarIcon
+        class='MaterialIcons'
+        focused={false}
+        name='adb'
+      />)
+    expect(wrapper).toMatchSnapshot()
+  })
+})
+
+describe('renders MaterialCommunityIcons/exponent correctly', () => {
+  it('should match Snapshot', () => {
+    const wrapper = mount(
+      <TabBarIcon
+        class='MaterialCommunityIcons'
+        focused={false}
+        name='exponent'
+      />)
+    expect(wrapper).toMatchSnapshot()
+  })
+})
+
+describe('renders Feather/aperture correctly', () => {
+  it('should match Snapshot', () => {
+    const wrapper = mount(
+      <TabBarIcon
+        class='Feather'
+        focused={false}
+        name='aperture'
+      />)
+    expect(wrapper).toMatchSnapshot()
+  })
+})
