36fa360b5696fb65c65e797bc63f07b3bdd7f9b3
JannisRex <rex.jannis@web.de>
2020-03-23 09:28:39 +0100

    [added] color prop, just copied whole thingand wrapped in if(color)
    Signed-off-by: JannisRex <rex.jannis@web.de>

--- src/components/Text/main.tsx
+++ src/components/Text/main.tsx
@@ -4,10 +4,36 @@ import styles from './styles'
 
 type Props = {
   children: string,
-  type?: string
+  type?: string,
+  color?: string
 }
 
-const customText = ({ children, type }: Props): JSX.Element => {
+const customText = ({ children, type, color }: Props): JSX.Element => {
+  if (color) {
+    if (type === 'title' || type === 'Title') {
+      return (
+        <Text style={[styles.textTitle, { color: color }]}>
+          {children}
+        </Text>
+      )
+    }
+
+    if (type === 'caption' || type === 'Caption') {
+      return (
+        <Text style={[styles.textCaption, { color: color }]}>
+          {children}
+        </Text>
+      )
+    }
+
+    return (
+      <Text style={[styles.textBody, { color: color }]}>
+        {children}
+      </Text>
+    )
+  }
+
+
   if (type === 'title' || type === 'Title') {
     return (
       <Text style={styles.textTitle}>
