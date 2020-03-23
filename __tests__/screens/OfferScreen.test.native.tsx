834ee378ed38d60142d631044dc9a1d1c455fdf6
JannisRex <rex.jannis@web.de>
2020-03-23 12:12:22 +0100

    [jest] passing isLoading:false to not render LoadingActivity, but page instead
    Signed-off-by: JannisRex <rex.jannis@web.de>

--- __tests__/screens/OfferScreen.test.native.tsx
+++ __tests__/screens/OfferScreen.test.native.tsx
@@ -1,10 +1,12 @@
 import React from 'react'
 import OfferScreen from '../../src/screens/offer/main.tsx'
 import { shallow } from 'enzyme'
+import "isomorphic-fetch"
 
 describe('renders OfferScreen correctly', () => {
   it('should match Snapshot', () => {
     const wrapper = shallow(<OfferScreen />)
+    wrapper.setState({ isLoading: false })
     expect(wrapper).toMatchSnapshot()
   })
 })
