2eb9ab0c9e407009e7c83eac4feb60afa2b074c7
JannisRex <rex.jannis@web.de>
2020-03-23 04:01:46 +0100

    [adjusted] a little bit
    Signed-off-by: JannisRex <rex.jannis@web.de>

--- src/screens/offer/main.tsx
+++ src/screens/offer/main.tsx
@@ -1,20 +1,28 @@
 import React, { Component } from 'react'
-import { View, Button } from 'react-native'
-import { Container, Header, Text } from '../../components/index'
+import { Button, View } from 'react-native'
+import { Container, Header } from '../../components/index'
 
 class OfferScreen extends Component {
+  constructor() {
+    super()
+
+    this.state = {
+      isBroken: false
+    }
+  }
+
+  _throwError: any = () => this.setState({ isBroken: true });
+
   render(): JSX.Element {
+    if (this.state.isBroken) {
+      throw new Error('Button killed it!')
+    }
+
     return (
       <Container>
         <Header />
-        <View style={{ flex: 1 }}>
-          <Text type='title'>Angebooooote</Text>
-          <Button
-            title='Throw Error!'
-            onPress={(): any => {
-              throw new Error('Button killed it!')
-            }}
-          />
+        <View style={{ flex: 1, justifyContent: 'center', alignItems: 'center' }}>
+          <Button onPress={this._throwError} title={'crash'} />
         </View>
       </Container>
     )
