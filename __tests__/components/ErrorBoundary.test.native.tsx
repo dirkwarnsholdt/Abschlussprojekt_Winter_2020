bdc5d484e45971c4322598c00cc874909dbe8996
JannisRex <rex.jannis@web.de>
2020-03-23 04:57:40 +0100

    [Jest] finished the fucking ErrorBoundary test =))))))
    Signed-off-by: JannisRex <rex.jannis@web.de>

--- __tests__/components/ErrorBoundary.test.native.tsx
+++ __tests__/components/ErrorBoundary.test.native.tsx
@@ -1,44 +1,67 @@
 import React from 'react'
 import { Text, View } from 'react-native'
 import renderer from 'react-test-renderer'
-import { mount } from 'enzyme'
 import { ErrorBoundary } from '../src/components/index'
 
 describe('ErrorBoundary', () => {
-  let ComponentWithoutError
-  let ComponentWithError
   let consoleErrorSpy
   let errorMock
+  let workingComponent
 
   beforeAll(() => {
-    ComponentWithoutError = () => null
-    ComponentWithError = () => { throw new Error()}
-    consoleErrorSpy = jest.spyOn(console, 'error').mockImplementation(() => {})
+    consoleErrorSpy = jest.spyOn(console, 'error').mockImplementation(() => {}) // to clear ErrorBoundary errors in cli
     errorMock = new Error('This is a test error!')
+    workingComponent = () => null
   })
 
   afterAll(() => {
-    consoleErrorSpy.mockRestore()
+    consoleErrorSpy.mockRestore() // called after to reset the consoleError ---- reset props to default
   })
 
-  it('should render children when there are no errors', () => {
-    const wrapper = mount(
+  it('should render children', () => {  // Default, everything is OK and children should get rendered
+    const wrapper = renderer.create(
       <ErrorBoundary>
-        <ComponentWithoutError />
+        <workingComponent />
       </ErrorBoundary>
     )
-    expect(wrapper.instance().state.hasError).toBe(false)
+    expect(wrapper.getInstance().state.error).toBe(null)
+    expect(wrapper.getInstance().state.hasError).toBe(false)
     expect(wrapper).toMatchSnapshot()
   })
 
-  it('should catch errors set the state and render the default FallbackComponent', () => {
-    const wrapper = mount(
+  it('should catch errors set the state and render the default FallbackComponent', () => { // checking if getDerivedStateFromError catches err
+    const wrapper = renderer.create(
       <ErrorBoundary>
-        <ComponentWithError />
+        <View>{errorMock}</View>
       </ErrorBoundary>
     )
-    expect(wrapper.instance().state.hasError).toBe(false)
+    expect(wrapper.getInstance().state.hasError).toBe(true)
     expect(wrapper).toMatchSnapshot()
   })
 
+  it('should catch errors set the state render the FallbackComponent and call onError()', () => {
+    const FallbackComponent = () => <Text>Error Fallback!</Text> // This is ErrSubstituteScreen
+    const onError = jest.fn() // just mocking the fucntion and test, if its getting called
+    const wrapper = renderer.create(
+      <ErrorBoundary FallbackComponent={FallbackComponent} onError={onError}>
+        <View>{errorMock}</View>
+      </ErrorBoundary>
+    )
+    expect(wrapper.getInstance().state.error).not.toBeNull()
+    expect(wrapper.getInstance().state.hasError).toBe(true)
+    expect(onError).toHaveBeenCalledWith(expect.any(Error), expect.any(String))
+    expect(wrapper).toMatchSnapshot()
+  })
+
+  it('should reset state when calling resetError', () => {
+    const wrapper = renderer.create(
+      <ErrorBoundary>
+        <workingComponent />
+      </ErrorBoundary>
+    )
+    wrapper.getInstance().setState({ error: errorMock, hasError: true }) // setting state manually
+    wrapper.getInstance().resetError()  // calling reset() to check functionality
+    expect(wrapper.getInstance().state.error).toBeNull()
+    expect(wrapper.getInstance().state.hasError).toBe(false)
+  })
 })
