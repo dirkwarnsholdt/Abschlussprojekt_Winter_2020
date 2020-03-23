be4e298523b2af18db44daa05d154d3d3728c46c
JannisRex <rex.jannis@web.de>
2020-03-23 07:13:55 +0100

    [Jest] fixed tests
    Signed-off-by: JannisRex <rex.jannis@web.de>

--- __tests__/screens/HomeScreen.test.native.tsx
+++ __tests__/screens/HomeScreen.test.native.tsx
@@ -2,7 +2,7 @@ import React from 'react'
 import HomeScreen from '../../src/screens/home/main.tsx'
 import { shallow } from 'enzyme'
 
-describe('renders MaterialIcons/adb correctly', () => {
+describe('renders HomeScreen correctly', () => {
   it('should match Snapshot', () => {
     const wrapper = shallow(<HomeScreen />)
     expect(wrapper).toMatchSnapshot()
