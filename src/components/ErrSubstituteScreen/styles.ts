a23b04189eace417b4b25cd87abef39d48146601
JannisRex <rex.jannis@web.de>
2020-03-23 01:14:10 +0100

    [TS] casting stylesheets as Textstyle to prevent fontWEight error
    Signed-off-by: JannisRex <rex.jannis@web.de>

--- src/components/ErrSubstituteScreen/styles.ts
+++ src/components/ErrSubstituteScreen/styles.ts
@@ -1,7 +1,7 @@
-import { StyleSheet } from 'react-native'
+import { StyleSheet, TextStyle } from 'react-native'
 import theme from '../../config/theme.style'
 
-const styles = StyleSheet.create({
+const styles: any = StyleSheet.create({
   errorHeader: {
     fontFamily: 'lato_lightitalic',
     fontSize: theme.FONT_SIZE_LARGE,
@@ -9,7 +9,7 @@ const styles = StyleSheet.create({
     fontWeight: theme.FONT_WEIGHT_THIN,
     color: theme.ERROR_HEADER,
     textAlign: 'center'
-  },
+  } as TextStyle,
   errorInfo: {
     fontFamily: 'lato_hairline',
     fontSize: theme.FONT_SIZE_SMALL,
@@ -17,12 +17,12 @@ const styles = StyleSheet.create({
     fontWeight: theme.FONT_WEIGHT_MEDIUM,
     color: theme.ERROR_TEXT,
     paddingVertical: 10
-  },
+  } as TextStyle,
   errorContainer: {
     flex: 1,
     marginHorizontal: 12,
     backgroundColor: theme.ERROR_BACKGROUND
-  }
+  } as TextStyle
 })
 
 export default styles
