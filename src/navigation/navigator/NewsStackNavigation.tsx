6413a234a3dce838fb0a2fc0bfe4257b1a353230
JannisRex <rex.jannis@web.de>
2020-03-23 08:26:11 +0100

    [TS] removed Props Typing
    Signed-off-by: JannisRex <rex.jannis@web.de>

--- src/navigation/navigator/NewsStackNavigation.tsx
+++ src/navigation/navigator/NewsStackNavigation.tsx
@@ -1,16 +1,10 @@
-import { Component } from 'react'
 import { createAppContainer } from 'react-navigation'
 import { createStackNavigator } from 'react-navigation-stack'
-
 import NewsScreen from '../../screens/news/main'
 import DetailScreen from '../../screens/detail/main'
 
-type Props = {
-  NewsScreen: Component<any>,
-  DetailScreen: Component<any>
-}
 
-const PeopleStackNavigation = createStackNavigator<Props>(
+const PeopleStackNavigation = createStackNavigator(
   {
     NewsScreen: {
       screen: NewsScreen,
--- src/navigation/navigator/OfferStackNavigation.tsx
+++ src/navigation/navigator/OfferStackNavigation.tsx
@@ -1,18 +1,11 @@
-import { Component } from 'react'
 import { createAppContainer } from 'react-navigation'
 import { createStackNavigator } from 'react-navigation-stack'
-
 import OfferScreen from '../../screens/offer/main'
 import OfferDetailScreen from '../../screens/offerDetail/main'
 import DetailScreen from '../../screens/detail/main'
 
-type Props = {
-  OfferScreen: Component<any>,
-  OfferDetailScreen: Component<any>,
-  DetailScreen: Component<any>
-}
 
-const OfferStackNavigation = createStackNavigator<Props>(
+const OfferStackNavigation = createStackNavigator(
   {
     OfferScreen: {
       screen: OfferScreen,
