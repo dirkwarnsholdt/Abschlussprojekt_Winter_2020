b1769168494618e7247329d399d0707805a27572
Jannis Rex <rex.jannis@web.de>
2020-04-01 10:33:50 +0200

    [TS] fixed typings
    Signed-off-by: Jannis Rex <rex.jannis@web.de>

--- src/lib/unescapes/nUnescape.tsx
+++ src/lib/unescapes/nUnescape.tsx
@@ -1,8 +1,8 @@
 import React from 'react'
 import { Text } from 'react-native'
 
-const nUnescape = (str: string): string => {
-  let newString = str
+const nUnescape = (str: string): JSX.Element => {
+  let newString
   try {
     newString = str.split(`\\n`).map((txt: string, key: number) => <Text key={key}>{ txt }{ '\n' }</Text>)
   } catch (e) { /* when there is no \n */ }
--- src/lib/unescapes/rUnescape.tsx
+++ src/lib/unescapes/rUnescape.tsx
@@ -1,8 +1,8 @@
 import React from 'react'
 import { Text } from 'react-native'
 
-const rUnescape = (str: string): string => {
-  let newString = str
+const rUnescape = (str: string): JSX.Element => {
+  let newString
   try {
     newString = str.split(`\\r`).map((txt: string, key: number) => <Text key={key}>{ txt }{ '\r' }</Text>)
   } catch (e) { /* when there is no \r */ }
