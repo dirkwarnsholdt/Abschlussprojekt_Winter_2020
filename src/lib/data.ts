dfa5bcfa7842bb64e7449e396593cce57ba6d7dc
Jannis Rex <rex.jannis@web.de>
2020-04-01 10:37:40 +0200

    [deleted]
    Signed-off-by: Jannis Rex <rex.jannis@web.de>

--- src/lib/data.ts
+++
@@ -1,7 +0,0 @@
-/* eslint-disable */
-// @ts-nocheck
-export default [
-  { id: 6, title: 'Abschlussfest der BvB', content: 'Liebe BvB-ler*innen, Mitarbeiter*innen und G\u00e4ste,\r\n\r\nwir laden Sie\/Euch herzlich zu unserem Abschlussfest am Dienstag, 25.6.2019 ein, um unseren Abschluss der BvB zu feiern und sch\u00f6n ausklingen zulassen.\r\n\r\nDas Fest beginnt um 13:00 Uhr im Freizeithaus.', edited_by: 2, created_at: '2019-05-08 14:49:29', updated_at: '2019-08-09 09:05:05' },
-  { id: 8, title: 'xxxxxxxxxxxxxxxxxx', content: 'Liebe BvB-ler*innen, Mitarbeiter*innen und G\u00e4ste,\r\n\r\nwir laden Sie\/Euch herzlich zu unserem Abschlussfest am Dienstag, 25.6.2019 ein, um unseren Abschluss der BvB zu feiern und sch\u00f6n ausklingen zulassen.\r\n\r\nDas Fest beginnt um 13:00 Uhr im Freizeithaus.', edited_by: 2, created_at: '2019-05-08 14:49:29', updated_at: '2019-08-09 09:05:05' },
-  { id: 11, title: 'yyyyyyyyyyyyyyyyyyyyy', content: 'Liebe BvB-ler*innen, Mitarbeiter*innen und G\u00e4ste,\r\n\r\nwir laden Sie\/Euch herzlich zu unserem Abschlussfest am Dienstag, 25.6.2019 ein, um unseren Abschluss der BvB zu feiern und sch\u00f6n ausklingen zulassen.\r\n\r\nDas Fest beginnt um 13:00 Uhr im Freizeithaus.', edited_by: 2, created_at: '2019-05-08 14:49:29', updated_at: '2019-08-09 09:05:05' },
-]
